46e25cba99fcac2ae04e5bde28997f1ab9b428e2
Ketian Yu <yukt@umich.edu>
2023-03-29 18:04:27 -0400

    documentation & timing

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 #include <getopt.h>
 #include "Analysis.h"
 
+void Version();
 void helpFile();
 
 int main(int argc, char ** argv)
@@ -14,30 +15,63 @@ int main(int argc, char ** argv)
                     {"vcf",    required_argument, nullptr, 'v'},
                     {"weight", required_argument, nullptr, 'w'},
                     {"output", required_argument, nullptr, 'o'},
+                    {"format", required_argument, nullptr, 'f'},
                     {"help",   no_argument      , nullptr, 'h'},
                     {nullptr,0,nullptr,0}
             };
 
-    while ((c = getopt_long(argc, argv, "v:w:o:h",loptions,nullptr)) >= 0)
+    while ((c = getopt_long(argc, argv, "v:w:o:f:h",loptions,nullptr)) >= 0)
     {
         switch (c) {
             case 'v': myUserVariables.VcfFileName = optarg;   break;
             case 'w': myUserVariables.WeightFileName = optarg;   break;
             case 'o': myUserVariables.OutputPrefix = optarg;   break;
+            case 'f': myUserVariables.Format = optarg;   break;
             case 'h': helpFile(); return 0;
             case '?': helpFile(); return 0;
             default: printf("[ERROR:] Unknown argument: %s\n", optarg);
         }
     }
 
+    if(!myUserVariables.CheckValidity()) return -1;
+
+    Version();
     Analysis myAnalysis(myUserVariables);
+    myAnalysis.myUserVariables->Status();
+
+    time_t start_time = time(nullptr);
     String MySuccessStatus = myAnalysis.Run();
+    if(MySuccessStatus!="Success")
+        return -1;
+    time_t time_tot = time(nullptr) - start_time;
+    cout << "\n Analysis finished." << endl;
+    cout << "   -- Analysis took " << time_tot << " seconds." << endl;
 
     return 0;
 }
 
+void Version()
+{
+    printf("\n ------------------------------------------------------------------------------ \n");
+    printf("             PRScal -- A Tool to Assemble Polygenic Risk Score    \n");
+    printf(" ------------------------------------------------------------------------------\n");
+    printf(" (c) 2023 - Ketian Yu, Goncalo Abecasis \n");
+    cout<< " Version : " << VERSION<< ";\n Built   : " << DATE << " by " << USER << endl;
+}
 
 void helpFile()
 {
-
+    Version();
+    printf( " About   : Calculate PRS from individual-level of genotypes data given weights.\n");
+    printf( " Usage   : PRScal [options] \n");
+    printf( " Options :\n");
+    printf( "   -v [dose.vcf.gz]               // [Required] Input Dosage File\n");
+    printf( "   -w [weights.gz]                // [Required] Input Weight File\n");
+    printf( "   -o [OutputPrefix]              // [Required] Output Prefix\n");
+    printf( "   --format [GT/DS/HDS]           // [Optional] Genotype info format (Default: DS)\n");
+    printf( "   -h, --help                     //  If ON, detailed help on options and usage. \n");
+    printf( " Note: the weight file should contain tab-separated columns:\n");
+    printf( "       \t\t#CHROM\t|\tBP\t|\tID\t|\tREF\t|\tALT\t|\tBETA\n");
+    printf( "       BETA field format is: gene1=weight1;gene2=weight2;...\n");
+    cout<<endl;
 }
--- src/UserVariables.h
+++ src/UserVariables.h
@@ -24,6 +24,44 @@ public:
         OutputPrefix = "";
         Format = "DS";
     };
+
+    void Status()
+    {
+        cout << " Command Line Options: " << endl;
+        printf( "   --vcf    [%s]\n", VcfFileName.c_str());
+        printf( "   --weight [%s]\n", WeightFileName.c_str());
+        printf( "   --output [%s]\n", OutputPrefix.c_str());
+        printf( "   --format [%s]\n", Format.c_str());
+    }
+
+    bool CheckValidity() const
+    {
+        if(VcfFileName == ""){
+            cout<< " Missing -v [--vcf], a required parameter.\n\n";
+            cout<< " Try -h [--help] for usage ...\n\n";
+            cout<< " Program Exiting ...\n\n";
+            return false;
+        }
+        if(WeightFileName == ""){
+            cout<< " Missing -w [--weight], a required parameter.\n\n";
+            cout<< " Try -h [--help] for usage ...\n\n";
+            cout<< " Program Exiting ...\n\n";
+            return false;
+        }
+        if(OutputPrefix == ""){
+            cout<< " Missing -o [--output], a required parameter.\n\n";
+            cout<< " Try -h [--help] for usage ...\n\n";
+            cout<< " Program Exiting ...\n\n";
+            return false;
+        }
+        if(Format!="DS" and Format!="GT" and Format!="HDS")
+        {
+            printf("ERROR !!! Invalid argument --format %s !!!\n", Format.c_str());
+            return false;
+        }
+
+        return true;
+    }
 };
 
 #endif //MAIN_CPP_USERVARIABLES_H
